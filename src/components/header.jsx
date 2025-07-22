eacc38c2fe0a9e4851a1d031b3af121f59e2033c
Arman <solaa8239@gmail.com>
2025-07-22 14:07:23 +0330

    img import bug fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
+import { pokemonHeaderImg } from "../assets/header.png";
+
 const Header = () => {
   return (
     <>
       <div className="md:w-1/2 sm:w-4/5 sm:px-0 w-full px-1">
-        <img className="w-full h-auto" src="../src/assets/header.png" alt="" />
+        <img className="w-full h-auto" src={pokemonHeaderImg} alt="" />
       </div>
     </>
   );
